f318ef40fbb0bf88cc38c54cc4259d1457079fe4
mrjak13 <justinkenney13@gmail.com>
2019-01-16 16:18:07 -0500

    add meet up types actions and views

--- app/controllers/meet_up_types_controller.rb
+++ app/controllers/meet_up_types_controller.rb
@@ -1,2 +1,25 @@
 class MeetUpTypesController < ApplicationController
+	def index
+		@meet_up_types = MeetUpType.all
+	end
+
+	def show
+		@meet_up_type = MeetUpType.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+		@meet_up_type = MeetUpType.find(params[:id])
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
