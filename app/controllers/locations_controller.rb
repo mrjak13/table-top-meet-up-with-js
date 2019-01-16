0ee3ba4d40c67c0e51ebbc47946b622b6bab0a68
mrjak13 <justinkenney13@gmail.com>
2019-01-16 16:17:06 -0500

    add location actions and views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,25 @@
 class LocationsController < ApplicationController
+	def index
+		@locations = Location.all
+	end
+
+	def show
+		@location = Location.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+		@location = Location.find(params[:id])
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
