07560d5853f2da694f366059a1b9b9f42fd37e87
mrjak13 <justinkenney13@gmail.com>
2019-01-16 16:17:46 -0500

    add meet up actions and views

--- app/controllers/meet_ups_controller.rb
+++ app/controllers/meet_ups_controller.rb
@@ -1,2 +1,25 @@
 class MeetUpsController < ApplicationController
+	def index
+		@meet_ups = MeetUp.all
+	end
+
+	def show
+		@meet_up = MeetUp.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+		@meet_up = MeetUp.find(params[:id])
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
