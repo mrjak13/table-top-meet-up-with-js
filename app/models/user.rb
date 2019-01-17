eb8ce5b17a7ca2dec4e97011b43c1e8c01089db9
mrjak13 <justinkenney13@gmail.com>
2019-01-17 18:45:22 -0500

    add helper methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,22 @@ class User < ApplicationRecord
   has_many :user_meet_ups
   has_many :meet_ups, through: :user_meet_ups
 
+  def add_game(game_name)
+  	game = Game.find_by(name: game_name)
+  	if self.games.include?(game)
+  		"#{game_name} is already in your library"
+  	else
+  		self.games << game
+  	end
+  end
+
+  def add_meet_up(meet_up)
+    # meet_up = MeetUp.find_by(name: meet_up_name)
+    if self.meet_ups.include?(meet_up)
+      "You are already attending #{meet_up.name}."
+    else
+      self.meet_ups << meet_up
+    end
+  end
+
 end
