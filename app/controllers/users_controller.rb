6c5d7648bda08ec367c43d2edc7507336d8a92e4
mrjak13 <justinkenney13@gmail.com>
2019-01-17 18:47:07 -0500

    able to update user with new meet_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,13 @@ class UsersController < ApplicationController
 	end
 
 	def edit
+
 	end
 
-	def update
+	def update		
+		meet_up = MeetUp.find(params[:meet_up_id])
+		current_user.add_meet_up(meet_up)		
+		redirect_to user_path(@current_user)
 	end
 
 	def destroy
