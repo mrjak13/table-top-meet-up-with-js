e9a4e39cdfa1663dee7086978a766c531a9a6745
mrjak13 <justinkenney13@gmail.com>
2019-01-17 18:50:13 -0500

    add helper to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,11 @@ class Location < ApplicationRecord
 	has_many :meet_up_types, through: :meet_ups
 
 	scope :near_me, -> (zip) { where("zip = ?", zip) }
+
+	def address
+		"#{self.address_1} " + "#{self.address_2}, " +
+		"#{self.city}, " +
+		"#{self.state}, " +
+		"#{self.zip}" 
+	end
 end
