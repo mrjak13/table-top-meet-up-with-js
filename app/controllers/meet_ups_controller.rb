6f1ba780c05f9c772fca2f6573bb334d4369e43c
mrjak13 <justinkenney13@gmail.com>
2019-01-18 15:44:15 -0500

    able to create new meet up, add helper method for date and time

--- app/controllers/meet_ups_controller.rb
+++ app/controllers/meet_ups_controller.rb
@@ -9,9 +9,23 @@ class MeetUpsController < ApplicationController
 	end
 
 	def new
+		@meet_up_types = MeetUpType.all
+		if params[:location_id]
+			@location = Location.find(params[:location_id])
+		end
 	end
 
 	def create
+		create_date_time
+		location = Location.find(params[:location_id])
+		meet_up = MeetUp.new(meet_up_params)
+		meet_up.update(date: @date_time, time: @date_time, location_id: location.id)
+
+		if meet_up.valid?			
+			redirect_to meet_up_path(meet_up)
+		else
+			redirect_to location_path(location)
+		end
 	end
 
 	def edit
@@ -22,5 +36,25 @@ class MeetUpsController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+
+	def meet_up_params
+		params.require(:meet_up).permit(
+			:name,
+			:meet_up_type_id,
+			:location_id
+			)
+	end
+
+	def create_date_time
+		year = params[:meet_up]["date(1i)"].to_i
+		month = params[:meet_up]["date(2i)"].to_i
+		day = params[:meet_up]["date(3i)"].to_i
+		hour = params[:meet_up]["time(4i)"].to_i
+		minute = params[:meet_up]["time(5i)"].to_i
+
+		@date_time = DateTime.new(year, month, day, hour, minute)
 	end
 end
