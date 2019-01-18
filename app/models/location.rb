2b760c2e9fd1613dad355ee64daad523ed2454b8
mrjak13 <justinkenney13@gmail.com>
2019-01-18 12:31:28 -0500

    add validations to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,12 @@ class Location < ApplicationRecord
 	has_many :meet_ups
 	has_many :meet_up_types, through: :meet_ups
 
+	validates :name, :address_1, :city, :state, :zip, presence: true
+	validates :name, uniqueness: true
+	validates :zip, length: { is: 5 }
+
+
+
 	scope :near_me, -> (zip) { where("zip = ?", zip) }
 
 	def address
