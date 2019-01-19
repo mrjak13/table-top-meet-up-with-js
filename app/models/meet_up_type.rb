5467456c7aeda720b54d35827e39acc969463a66
mrjak13 <justinkenney13@gmail.com>
2019-01-19 11:37:32 -0500

    add validations to meet up type model

--- app/models/meet_up_type.rb
+++ app/models/meet_up_type.rb
@@ -1,4 +1,7 @@
 class MeetUpType < ApplicationRecord
 	has_many :meet_ups
 	has_many :locations, through: :meet_ups
+
+	validates :name, uniqueness: true
+	validates :name, :description, presence: true
 end
