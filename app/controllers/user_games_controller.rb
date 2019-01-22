29e2fb1a0aee588d728047d7e878d913f999ac58
mrjak13 <justinkenney13@gmail.com>
2019-01-22 13:50:13 -0500

    able to add/remove favorites and delete game from library

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,14 +1,24 @@
 class UserGamesController < ApplicationController
   	def create  	
   		game = Game.find(params[:game_id])  	
-  		current_user.add_game(game)  	
-  		redirect_to user_path(@current_user)
+  		current_user.add_game(game)
+
+  		redirect_to game_path(game)
   	end
 
   	def update
+  		@user_game = UserGame.find(params[:id])
+  		@user_game.favorite == false ? @user_game.favorite = true : @user_game.favorite = false
+  		@user_game.save 
+
+  		redirect_to game_path(@user_game.game)
   	end
 
   	def destroy
+  		@user_game = UserGame.find(params[:id])
+  		@user_game.destroy
+  		
+  		redirect_to game_path(@user_game.game)
   	end 
 
 end
