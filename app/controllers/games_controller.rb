1035e337a11ee761153666d0313f9d572d83a7eb
mrjak13 <justinkenney13@gmail.com>
2019-01-22 10:06:22 -0500

    able to create new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,6 +11,12 @@ class GamesController < ApplicationController
 	end
 
 	def create
+		@game = Game.create(games_params)
+		if @game.valid?
+			redirect_to game_path(@game)
+		else
+			redirect_to new_game_path
+		end
 	end
 
 	def edit
@@ -21,5 +27,18 @@ class GamesController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+
+	def games_params
+		params.require(:game).permit(
+			:name,
+			:description,
+			:min_players,
+			:max_players,
+			:play_time,
+			:mature_content
+			)
 	end
 end
