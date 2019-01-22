cbbffd29809d751a77e6881ebdb9aa00f94c3dcf
mrjak13 <justinkenney13@gmail.com>
2019-01-22 10:06:00 -0500

    add validations to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,10 @@ class Game < ApplicationRecord
 	has_many :user_games
 	has_many :users, through: :user_games
 
+	validates :name, :min_players, :max_players, :description, :play_time, presence: true
+	validates :name, uniqueness: true
+
+
 	def players
 		self.min_players.to_s + "-" + self.max_players.to_s 
 	end
