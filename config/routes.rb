07ed7f35cdbb645f1c0d2c15e2d0180481e03947
mrjak13 <justinkenney13@gmail.com>
2019-01-23 12:08:00 -0500

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,27 +5,36 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'  
 
+
+  resources :users, except: [:create]
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   resources :meet_up_types
 
+  resources :user_meet_ups, only: [:destroy]
+
+  resources :user_games, only: [:update, :destroy]
+
+  # nested routes
   resources :locations do 
     resources :meet_ups, only: [:new, :create]
   end
+
+  resources :users do
+    resources :games, only: [:index]
+  end
   
+  # nested routes for join tables
   resources :meet_ups do
     resources :user_meet_ups, only: [:create]
   end
 
-  resources :user_meet_ups, only: [:destroy]
-
   resources :games do
-  	  resources :user_games, only: [:create] 
-  	end
+      resources :user_games, only: [:create] 
+  end
 
-  resources :user_games, only: [:update, :destroy]
-
-  resources :users, except: [:create]
-  get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
