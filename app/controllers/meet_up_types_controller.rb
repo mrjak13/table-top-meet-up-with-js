ba9a108f7d2bb06652e70bdaf928384b1b706ea2
mrjak13 <justinkenney13@gmail.com>
2019-01-23 23:47:35 -0500

    add partial and ability to edit

--- app/controllers/meet_up_types_controller.rb
+++ app/controllers/meet_up_types_controller.rb
@@ -1,14 +1,14 @@
 class MeetUpTypesController < ApplicationController
 	before_action :must_be_logged_in, except: [:index]
-	before_action :current_user, only: [:index]
+	before_action :current_user, only: [:index, :show]
 	before_action :must_be_admin, except: [:index]
+	before_action :assign_meet_up_type, only: [:show, :edit, :update]
 
 	def index
 		@meet_up_types = MeetUpType.all
 	end
 
 	def show
-		@meet_up_type = MeetUpType.find(params[:id])
 	end
 
 	def new
@@ -25,10 +25,15 @@ class MeetUpTypesController < ApplicationController
 	end
 
 	def edit
-		@meet_up_type = MeetUpType.find(params[:id])
 	end
 
 	def update
+		@meet_up_type.update(meet_up_type_params)
+		if @meet_up_type.valid?
+			redirect_to meet_up_type_path(@meet_up_type)
+		else
+			redirect_to edit_meet_up_type_path
+		end
 	end
 
 	def destroy
@@ -39,4 +44,9 @@ class MeetUpTypesController < ApplicationController
 	def meet_up_type_params
 		params.require(:meet_up_type).permit(:name, :description)
 	end
+
+	def assign_meet_up_type
+		@meet_up_type = MeetUpType.find(params[:id])
+	end
+
 end
