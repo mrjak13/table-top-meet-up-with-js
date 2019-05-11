17312455d176c22e699a72fe8bf01d68438dae85
mrjak13 <justinkenney13@gmail.com>
2019-05-11 11:54:11 -0400

    finish ajax call to location show page, add location constructor and function

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -34,7 +34,11 @@ function getLocation() {
 }
 
 function locationHtml(location) {
-	return `<li><a href="/locations/${location.id}">${location.name}</a><ul id="location id ${location.id}"><ul></li>`
+	return `
+	<li>
+		<a href="/locations/${location.id}">${location.name}</a>
+		<ul id="location id ${location.id}"><ul>
+	</li>`
 }
 
 
@@ -45,6 +49,39 @@ function rmvButton() {
 function locationLinkClick() {
 	$('#ajax-list-locations a').on('click', function(e) {	
 		e.preventDefault();
-		debugger;
+		getLocationShow(this.href);
 	})
+}
+
+function getLocationShow(url) {
+	$.get(`${url}.json`, function(location){
+		var newLocation = new Location(location)
+		var locationHTML = newLocation.formatHTML();
+		addLocatoinHTMLToDom(location.id, locationHTML)
+	})
+}
+
+function Location(obj) {
+
+	this.id = obj.id
+	this.name = obj.name
+	this.address_1 = obj.address_1
+	this.address_2 = obj.address_2
+	this.city = obj.city
+	this.state = obj.state
+	this.zip = obj.zip
+	this.meetUps = obj.meet_ups
+
+}
+
+Location.prototype.formatHTML = function() {
+	return `
+	<p>${this.address_1}, ${this.city}, ${this.zip} ${this.address_2}</p>
+	<br>
+	<p>Number of events<strong>${this.meetUps.length}</strong></p>
+	`
+}
+
+function addLocatoinHTMLToDom(id, html) {
+	debugger;
 }
