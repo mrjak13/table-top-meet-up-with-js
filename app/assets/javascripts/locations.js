60578cb7353d6ed2b7d37c7260ce6d5aef6ecbe1
mrjak13 <justinkenney13@gmail.com>
2019-05-11 10:45:08 -0400

    fix bug when ajax call to locations index, add links to dom on click, start on link click

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -9,16 +9,42 @@ function locationsClick() {
 		event.preventDefault();
 		console.log('clicked a thing in my project woo!');
 		getLocation();
+		rmvButton();
 	})
 }
 
 function getLocation() {
+	// 
+	// COULD NOT GET AJAX REQUEST TO WORK W/O .JSON.. TRY WITH MEET UP TYPES 
+	// 
+	// $.ajax({
+	// 	method: "GET",
+	// 	url: "http://localhost:3000/locations",
+	// 	datatype: "json"
+	// }).done(function (data) {
+	// 	debugger;
+	// })
+	$.get('/locations.json', function(data){
+		data.forEach(function(location){
+			let html = locationHtml(location)
+			$('#ajax-list-locations').append(html)
+		})
+		locationLinkClick();
+	})
+}
 
-	$.ajax({
-		url: 'http://localhost:3000/locations',
-		method: 'get',
-		datatype: 'json'
-	}).done(function (data) {
+function locationHtml(location) {
+	return `<li><a href="/locations/${location.id}">${location.name}</a><ul id="location id ${location.id}"><ul></li>`
+}
+
+
+function rmvButton() {
+	$('button#ajax-get-locations').remove();
+}
+
+function locationLinkClick() {
+	$('#ajax-list-locations a').on('click', function(e) {	
+		e.preventDefault();
 		debugger;
 	})
 }
